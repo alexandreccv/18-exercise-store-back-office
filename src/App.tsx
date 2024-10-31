79580a12507524a75204e3f906994e44e45502f7
Alexandre Carvalho <alex380bb@gmail.com>
2024-10-31 16:39:52 -0300

    req3,4

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import './styles/App.css';
+import { useState } from 'react';
 import ListProducts from './components/ListProducts';
 import RegisterProduct from './components/RegisterProduct';
 
 function App() {
+  const [active, setActive] = useState('');
+
   return (
     <div className="app">
       <header>
-        <button>Cadastrar</button>
-        <button>Ver produtos</button>
+        <button onClick={ () => setActive('register') }>Cadastrar</button>
+        <button onClick={ () => setActive('list') }>Ver produtos</button>
       </header>
-      <RegisterProduct />
-      <ListProducts products={ [] } />
+      {active === 'register' && <RegisterProduct />}
+      {active === 'list' && <ListProducts products={ [] } />}
+
     </div>
   );
 }
